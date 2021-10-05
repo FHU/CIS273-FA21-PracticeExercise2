1cf5dd1067541e5382983cebb53c2b2e63595e91
Kenan Casey <kenancasey@gmail.com>
2021-10-05 15:37:36 -0500

    initial commit

---
+++ PracticeExercise2/IList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace PracticeExercise2
+{
+    public interface IList<T>
+    {
+        int Length { get; }
+
+        bool IsEmpty { get; }
+
+        // TODO
+        T First { get; }
+        // TODO
+        T Last { get; }
+
+        // Add i to end of list
+        void Append(T item);
+
+        // Add i to beginning of list
+        void Prepend(T item);
+
+        // Insert new value after first instance of exisiting value
+        // If existing value isn't found, then append to list.
+        void InsertAfter(T newValue, T existingValue);
+
+        // TODO
+        void InsertAt(T newValue, T index);
+
+        int FirstIndexOf(T existingValue);
+
+        // TODO
+        void Remove(T value);
+
+        // TODO
+        void RemoveAt(int index);
+
+        // TODO
+        void Clear();
+
+        // TODO
+        IList<T> Reverse();
+    }
+}
---
+++ PracticeExercise2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IList<int> list = new LinkedList<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Append(i);
+            }
+
+            Console.WriteLine(list);
+
+            list.Remove(0);
+
+            Console.WriteLine(list);
+
+            list.Remove(4);
+
+            Console.WriteLine(list);
+
+            list.Remove(3);
+
+            Console.WriteLine(list);
+
+            list.Remove(3);
+
+            Console.WriteLine(list);
+
+            list.Remove(2);
+
+            Console.WriteLine(list);
+
+            list.Remove(1);
+
+            Console.WriteLine(list);
+
+            Console.ReadKey();
+
+        }
+    }
+}
